8d83c23f0403539581c2a9bd653e13fae5045a24
pandabr <guh.clemente@gmail.com>
2019-01-13 16:28:30 -0200

    db wfs

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=127.0.0.1;dbname=Exemplo',
-    'username' => 'root',
-    'password' => 'root',
+    'dsn' => 'mysql:host=localhost;dbname=Exemplo',
+    'username' => 'tutorial',
+    'password' => 'WFStuto2019@',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
